c102cf0b984f0007f2cd1f7be319825792816cc1
Eduard Fray <eduard.fray@hotmail.de>
2025-01-28 16:35:25 +0100

    translation

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
